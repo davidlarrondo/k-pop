34e9f575163caf678e7a7cf04f1332e3697859e6
davidlarrondo <81131837+davidlarrondo@users.noreply.github.com>
2026-06-11 14:45:25 -0400

    primera carga proyecto calculadora
    Acción sumar del proyecto otaku de la Havhy

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def sumar(v1,v2):
+    resultado = v1+v2
+    return resultado
+
+valor1 = int(input("ingrese valor "))
+valor2 = int(input("ingrese valor "))
+
+print(f"el resultado es {sumar(valor1,valor2)}")
