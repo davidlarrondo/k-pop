93475ada3730a14cbdc67e00474bd07eac8a2ae7
davidlarrondo <81131837+davidlarrondo@users.noreply.github.com>
2026-06-11 14:53:33 -0400

    incorporación de función restar
    se incorporar función con retorno e invocación en main

--- calculadora.py
+++ calculadora.py
@@ -2,7 +2,12 @@ def sumar(v1,v2):
     resultado = v1+v2
     return resultado
 
+def restar(v1,v2):
+    resultado = v1-v2
+    return resultado
+
 valor1 = int(input("ingrese valor "))
 valor2 = int(input("ingrese valor "))
 
 print(f"el resultado es {sumar(valor1,valor2)}")
+print(f"el resultado es {restar(valor1,valor2)}")
